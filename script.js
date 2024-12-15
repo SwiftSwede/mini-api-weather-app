8aaa45abe5afb8faad5cc78be48e16ed626ae1dc
SwiftSwede <swift-swede@hotmail.com>
2024-12-15 17:08:30 -0500

    Added a loading spinner

--- script.js
+++ script.js
@@ -11,13 +11,26 @@ async function getWeather(location) {
 
 async function fetchWeather() {
   const location = document.getElementById("locationInput").value;
+  const weatherDisplay = document.getElementById("weatherDisplay");
   if (location) {
+    weatherDisplay.style.display = "flex";
+    weatherDisplay.innerHTML = `
+    <div>
+      <div class="spinnerContainer">
+      <p>Loading...</p>
+       <div class="spinner">
+       </div>
+      </div>
+    </div>
+  `;
     try {
       const weatherData = await getWeather(location);
       console.log(weatherData);
       displayWeather(weatherData);
     } catch (error) {
       console.error("Error fetching weather data:", error);
+      weatherDisplay.innerHTML =
+        "<p>Error fetching weather data. Please try again.</p>";
     }
   } else {
     alert("Please enter a location!");
@@ -26,6 +39,7 @@ async function fetchWeather() {
 
 function displayWeather(weatherData) {
   const weatherDisplay = document.getElementById("weatherDisplay");
+  const weatherText = document.getElementById("weatherText");
   const temperature = (
     ((weatherData.currentConditions.temp - 32) * 5) /
     9
@@ -39,3 +53,23 @@ function displayWeather(weatherData) {
 
   weatherDisplay.classList.add("visible");
 }
+
+const img = document.querySelector("img");
+
+/*
+1. Await doesn't work on global scope, so we must wrap it in a function
+2. We must assign await responses to variables in order to use them
+3. We need to call the function in order to use the data our async function got
+*/
+// async function getCats() {
+//   const response = await fetch(
+//     "https://api.giphy.com/v1/gifs/translate?api_key=C6sa12xa3W0bcC4vLy2SztWnUuxlfIvQ&s=cats",
+//     {
+//       mode: "cors",
+//     }
+//   );
+//   const catData = await response.json();
+//   img.src = catData.data.images.original.url;
+// }
+
+// getCats();
